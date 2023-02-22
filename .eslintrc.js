dea3c9c4a6e3bd9efcfd67a96782c4d92ef7175c
darknoll <darknoll@126.com>
2023-02-22 16:42:24 +0800

    fix(custom): 去除type:module

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: 'vue-eslint-parser',
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+
+  rules: {
+    //'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+  },
+}
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,15 +1,15 @@
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore({
-    id: 'user',
-    state: () => {
-        return {
-            name: '张三',
-        }
+  id: 'user',
+  state: () => {
+    return {
+      name: '张三',
+    }
+  },
+  actions: {
+    updateName(name: string) {
+      this.name = name
     },
-    actions: {
-        updateName(name: string) {
-            this.name = name
-        },
-    },
+  },
 })
