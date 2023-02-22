a1c50406c05173aeec746cf4147de9e086e122ca
darknoll <darknoll@126.com>
2023-02-22 16:40:27 +0800

    feature(custom): 初始化结构

---
+++ src/components/usePinia.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/store/user'
+
+const useStore = useUserStore()
+
+useStore.updateName('李四')
+</script>
---
+++ src/views/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="logo">测试 vueUse 的鼠标坐标</h1>
+  <h3>Mouse: {{ x }} x {{ y }}</h3>
+  <el-row class="mb-4">
+    <el-button disabled>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info" disabled>Info</el-button>
+    <el-button type="warning" disabled>Warning</el-button>
+    <el-button type="danger" disabled>Danger</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button plain disabled>Plain</el-button>
+    <el-button type="primary" plain disabled>Primary</el-button>
+    <el-button type="success" plain disabled>Success</el-button>
+    <el-button type="info" plain disabled>Info</el-button>
+    <el-button type="warning" plain disabled>Warning</el-button>
+    <el-button type="danger" plain disabled>Danger</el-button>
+  </el-row>
+</template>
+
+<script lang="ts" setup>
+import { useMouse } from '@vueuse/core'
+
+const { x, y } = useMouse()
+</script>
+
+<style lang="scss">
+.logo {
+  background-color: red;
+}
+</style>
